23639c38e0f317f468a689aeff0ecc335e623c1a
abdelouahedd <ennouriabdelouahed@gmail.com>
2020-12-18 10:59:36 +0100

    add spinner

--- front/front-react/src/App.js
+++ front/front-react/src/App.js
@@ -2,18 +2,22 @@ import { Route, Switch, BrowserRouter } from "react-router-dom";
 import Home from "./component/home/Home";
 import Login from "./component/Authentication/loginForm/Login"
 import PrivateRoute from "./routes/privateRoute/PrivateRoute"
+import { Suspense } from "react";
+import Spinner from "./component/shared/spinner/Spinner";
 
 function App() {
 
   return (
 
-    <BrowserRouter>
-      <Switch>
-        <Route path='/login' component={Login} />
-        <PrivateRoute path='/' component={Home} />
-        {/* <Route component={Page404} /> */}
-      </Switch>
-    </BrowserRouter>
+    <Suspense fallback={<Spinner />}>
+      <BrowserRouter>
+        <Switch>
+          <Route path='/login' component={Login} />
+          <PrivateRoute path='/' component={Home} />
+          {/* <Route component={Page404} /> */}
+        </Switch>
+      </BrowserRouter>
+    </Suspense>
   );
 }
 
